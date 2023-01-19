3b66ef9e6290b4470f40d0505f6f2cd1cce4ad08
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2023-01-19 08:55:05 -0600

    more code for json data

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 "use strict";
 
 import time from './data.json' assert { type: 'json'};
-
 console.log(time);
+
+var dailyWorkCurrent = time[0].timeframes.daily.current;
+var dailyWorkPrevious = time[0].timeframes.daily.previous;
+console.log(dailyWorkCurrent);
+console.log(dailyWorkPrevious);
 
 const dailyButton = document.getElementById("daily");
 const weeklyButton = document.getElementById("weekly");
@@ -13,12 +17,21 @@ weeklyButton.addEventListener("click", weeklySwitch);monthlyButton.addEventListe
 
 function dailySwitch() {
     alert("daily");
+    dailyButton.style.color = "white";
+    weeklyButton.style.color = "hsl(235, 45%, 61%)";
+    monthlyButton.style.color = "hsl(235, 45%, 61%)";
 }
 
 function weeklySwitch() {
     alert("weekly");
+    weeklyButton.style.color = "white";
+    dailyButton.style.color = "hsl(235, 45%, 61%)";
+    monthlyButton.style.color = "hsl(235, 45%, 61%)";
 }
 
 function monthlySwitch() {
     alert("monthly");
+    monthlyButton.style.color = "white";
+    dailyButton.style.color = "hsl(235, 45%, 61%)";
+    weeklyButton.style.color = "hsl(235, 45%, 61%)";
 }
