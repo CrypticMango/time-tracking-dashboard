83ac64aca6e9e12caa13acbfbca1e359e6db128b
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2023-01-19 07:14:05 -0600

    started on functions for switching information

---
+++ app.js
@@ -0,0 +1,18 @@
+const dailyButton = document.getElementById("daily");
+const weeklyButton = document.getElementById("weekly");
+const monthlyButton = document.getElementById("monthly");
+
+dailyButton.addEventListener("click", dailySwitch);
+weeklyButton.addEventListener("click", weeklySwitch);monthlyButton.addEventListener("click", monthlySwitch);
+
+function dailySwitch() {
+    alert("daily");
+}
+
+function weeklySwitch() {
+    alert("weekly");
+}
+
+function monthlySwitch() {
+    alert("monthly");
+}
